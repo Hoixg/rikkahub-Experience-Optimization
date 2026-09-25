7356585f0ec23e8eb0a4115aad08884dfba53996
Hoixg <Hoixg@users.noreply.github.com>
2026-09-25 13:50:17 +0800

    chore: release 2.5.45

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "me.rerere.rikkahub.custom"
         minSdk = 26
         targetSdk = 37
-        versionCode = 189
-        versionName = "2.5.4"
+        versionCode = 190
+        versionName = "2.5.45"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
